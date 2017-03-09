bc3922981aa929a46dcf76fd09950867a1a34fda
chaochen <orangercc@gmail.com>
2017-03-09 09:53:54 +0800

    download one by one, fix nignix timeout

--- youtube2baiduyun/views.py
+++ youtube2baiduyun/views.py
@@ -3,13 +3,35 @@ from django.views.decorators.csrf import csrf_protect
 from django.contrib import messages
 
 import youtube_dl
-import os, sys
-import bypy
+import sys
 import subprocess
+
+# syn to baiduyun module
+
+
+def syn_baiduyun(request):
+    # local test
+    if sys.platform == 'darwin':
+        subprocess.call("bypy -v syncup /Users/chaochen/Dropbox/project/env_Django_Demo/video_crawler/download/ /",
+                        shell=True)
+        messages.success(request, 'syn down start to delete download file')
+
+        subprocess.call(
+            "rm -rf /Users/chaochen/Dropbox/project/env_Django_Demo/video_crawler/download/*", shell=True)
+        messages.success(request, 'action down!plz check baiduyun')
+
+    # for server
+    elif sys.platform == 'linux':
+        subprocess.call(
+            "bypy -v syncup /home/video_crawler/download/ /", shell=True)
+        messages.success(request, 'syn down start to delete download file')
+
+        subprocess.call("rm -rf /home/video_crawler/download/*", shell=True)
+        messages.success(request, 'action down!plz check baiduyun')
 
 
 # online video downloader module
-def string_ydl(file):
+def string_ydl(file, request):
     for line in file:
         # print(url)
         url = line.decode(encoding='UTF-8')
@@ -29,26 +51,8 @@ def string_ydl(file):
         }
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
-
-
-# syn to baiduyun module
-def syn_baiduyun(request):
-    # local test
-    if sys.platform == 'darwin':
-        subprocess.call("bypy -v syncup /Users/chaochen/Dropbox/project/env_Django_Demo/video_crawler/download/ /",
-                        shell=True)
-        messages.success(request, 'syn down start to delete download file')
-
-        subprocess.call("rm -rf /Users/chaochen/Dropbox/project/env_Django_Demo/video_crawler/download/*", shell=True)
-        messages.success(request, 'action down!plz check baiduyun')
-
-    # for server
-    elif sys.platform == 'linux':
-        subprocess.call("bypy -v syncup /home/video_crawler/download/ /", shell=True)
-        messages.success(request, 'syn down start to delete download file')
-
-        subprocess.call("rm -rf /home/video_crawler/download/*", shell=True)
-        messages.success(request, 'action down!plz check baiduyun')
+        messages.success(request, 'youtube %(title)s download finished.')
+        syn_baiduyun(request)
 
 
 @csrf_protect
@@ -62,12 +66,10 @@ def index(req):
             messages.error(req, 'error no file upload')
             return render(req, 'index.html')
 
-        string_ydl(uploadfile)
+        string_ydl(uploadfile, req)
 
-        messages.success(req, 'youtube download finished.')
+        # messages.success(req, 'start to syn to baiduyun.')
 
-        messages.success(req, 'start to syn to baiduyun.')
-
-        syn_baiduyun(req)
+        # syn_baiduyun(req)
 
     return render(req, 'index.html')
